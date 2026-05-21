1e331c194a1b07c5ab1a76670d6b59827c28220a
hugo0190. <hugo_gramme@estudante.sesisenai.org.br>
2026-05-21 09:08:59 -0300

    ADD- adicionei os arquivos

---
+++ db/script.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE sistema_simples;
+
+
+USE sistema_simples;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR (255)  NOT NULL,
+    password VARCHAR (255) NOT NULL,  
+
+);
+
+INSERT INTO users ( username, password) VALUES ('admin,'123');
