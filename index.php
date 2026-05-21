79e629a8ef4fdde7cd5bdb69060eb9030eead91b
hugo0190. <hugo_gramme@estudante.sesisenai.org.br>
2026-05-21 09:34:06 -0300

    add- fiz o php

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+
+
+session_start();
+
+
+$host = "local host";
+$user = "root";
+$pass = "";
+$db = "Sistema_simples";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($com -> connect_error){
+
+echo"<script> console.log('erro na conexao cpm o banco')</script>";
+
+
+}else{
+
+echo "<script> console.log('conexão com o banco foi um sucesso!')</script>";
+
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,5 +47,14 @@
 </head>
 <body>
     <h1>Tela de login PHP</h1>
+    <form method="POST">
+
+<label>Usuario<label>
+<input type="text"> name="Usuario"> <br>
+<label> Senha</label>
+<input type= "password" name="senha"> <br>
+
+<button type+ "submit"> Entrar</button>
+</form>
 </body>
 </html>
