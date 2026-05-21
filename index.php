0c46890cc4dfaedfa785debf760ea1e896d9ff83
hugo0190. <hugo_gramme@estudante.sesisenai.org.br>
2026-05-21 09:40:26 -0300

    fix- arrumei errs

--- index.php
+++ index.php
@@ -1,60 +1,41 @@
 <?php
 
-
 session_start();
-
-
-$host = "local host";
+$host = "localhost";
 $user = "root";
-$pass = "";
-$db = "Sistema_simples";
+$pass = "root";
+$db = "sistemaS_simples";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-if ($com -> connect_error){
-
-echo"<script> console.log('erro na conexao cpm o banco')</script>";
-
+if($conn -> connect_error){
+    echo "<script> console.log('erro na canexão com banco')</script>";
 
 }else{
 
-echo "<script> console.log('conexão com o banco foi um sucesso!')</script>";
-
+    echo "<script> console.log ('conexão com o banco foi um sucesso')</script>";
 }
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de login</title>
+    <title>tela de Login</title>
 </head>
 <body>
-    <h1>Tela de login PHP</h1>
+
+    <h1>Tela de Login - PHP</h1>
+
     <form method="POST">
 
-<label>Usuario<label>
-<input type="text"> name="Usuario"> <br>
-<label> Senha</label>
-<input type= "password" name="senha"> <br>
+        <label>Usuario</label>
+        <input type="text" name="usuario"> <br>
 
-<button type+ "submit"> Entrar</button>
-</form>
+        <label>Senha</label>
+        <input type="password" name="senha"> <br>
+
+        <button type="submit">Entrar</button>
+    </form>
 </body>
 </html>
