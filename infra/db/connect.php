adadf62eceaf1d21be6a7f8c1705703d013880b5
hugo0190. <hugo_gramme@estudante.sesisenai.org.br>
2026-06-02 08:55:44 -0300

    add- connect.php explicando

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,15 +1,29 @@
 <?php
 
+    // Define o endereço do servidor do banco de dados
     $host = "localhost";
+
+    // Define o nome do usuário que terá acesso ao banco
     $user = "root";
+
+    // Define a senha do usuário do banco de dados
     $pass = "root";
+
+    // Define o nome do banco de dados que será utilizado
     $db = "sistema_simples";
 
-    $conn = new mysqli($host,$user,$pass,$db);
+    // Cria uma nova conexão com o banco de dados MySQL
+    $conn = new mysqli($host, $user, $pass, $db);
 
+    // Verifica se ocorreu algum erro durante a conexão
     if ($conn->connect_error){
+
+        // Exibe uma mensagem de erro no console do navegador
         echo "<script> console.log('erro na conexão com o banco') </script>";
+
     }else{
+
+        // Exibe uma mensagem de sucesso no console do navegador
         echo "<script> console.log('conexão com o banco foi um sucesso')</script>";
     }
 
