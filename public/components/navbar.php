7bcce2329a2788dabe6cc1c860145a2a3e22d53e
hugo0190. <hugo_gramme@estudante.sesisenai.org.br>
2026-06-02 08:53:57 -0300

    add- navbar.php explicando

--- public/components/navbar.php
+++ public/components/navbar.php
@@ -1,7 +1,12 @@
-
-
+<!-- Cabeçalho principal da página -->
 <header>
+
+    <!-- Barra de navegação do sistema -->
     <nav>
-        <h2> Sistema Simples </h2>
+
+        <!-- Título exibido na barra de navegação -->
+        <h2>Sistema Simples</h2>
+
     </nav>
+
 </header>
