11b5c57bee43f45df1670d052e1f57a5dc281232
hugo0190. <hugo_gramme@estudante.sesisenai.org.br>
2026-06-02 08:47:59 -0300

    add- logout.php esplicação

--- public/logout.php
+++ public/logout.php
@@ -1,9 +1,15 @@
 <?php
 
+    // Inicia a sessão atual para que ela possa ser manipulada
     session_start();
+
+    // Remove todos os dados armazenados na sessão do usuário
     session_destroy();
-     
+
+    // Redireciona o usuário para a página de login (index.php)
     header("Location: ../index.php");
+
+    // Encerra a execução do script após o redirecionamento
     exit();
 
 ?>
